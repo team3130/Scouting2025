5d2acd1d7a37b62fb92367892bad1d5770c54ff5
Isaac Seel <IsaacCA2018@mgmail.com>
2025-05-07 19:39:30 -0500

    fixed a couple of things

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -54,14 +54,14 @@ var config_data = `
       "min": 1,
       "max": 99999
     },
-    { "name": "Auto Start Position",
+    { "name": "Auto start position",
       "code": "as",
-      "type": "clickable_image",
-      "filename": "2025/half_field.png",
-      "clickRestriction": "one",
-      "dimensions": "6 6",
-      "allowableResponses": "1 7 13 19 25 31",
-      "shape": "circle 5 black red true"
+      "type": "radio",
+      "choices": {
+        "s": "Barge Side<br>",
+        "f": "Middle<br>",
+        "b": "Processor Side"
+      }
     }
   ],
   "auton": [
@@ -101,14 +101,13 @@ var config_data = `
       "code": "ac4b",
       "type": "counter"
     },
-    { "name": "Pickup From",
-      "code": "tpu",
-      "type": "radio",
-      "choices": {
-        "s": "Barge Side<br>",
-        "f": "Middle<br>",
-        "b": "Processor Side"
-      }
+    { "name": "Auto Scoring Position",
+      "code": "asp",
+      "type": "clickable_image",
+      "filename": "2025/reef.png",
+      "dimensions": "6 6",
+      "allowableResponses": "1 2 3 4 5 6 7 8 9 10 11 12 13 14 17 18 19 20 23 24 25 26 27 28 29 30 31 32 33 34 35 36",
+      "shape": "circle 5 black red true"
     },
     { "name": "Processor Score",
       "code": "aps",
